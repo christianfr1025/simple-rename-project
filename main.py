eab5241c9b883a32d8d017f53f4c42a88d8a42e4
christianfr1025 <53807444+christianfr1025@users.noreply.github.com>
2022-11-09 19:11:58 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pathlib
 
 def rename_files_folders(base_dir):
     for item in pathlib.Path(base_dir).iterdir():
+        print("Checking " + os.path.join(item.parent, item.name))
         if not item.stem.startswith('.'):
             if item.is_file():
                 if item.stem != item.stem.upper():
@@ -12,7 +13,7 @@ def rename_files_folders(base_dir):
                 if item.name != item.name.upper():
                     print(f"Renaming \n '{item}' to \n'{os.path.join(item.parent, item.name.upper())}' \n")
                     item.rename(os.path.join(item.parent, item.name.upper()))
-                if item.name.find(".lr") == -1 or item.name.find(".LR") == -1:
+                if item.name.find(".lr") == -1 and item.name.find(".LR") == -1:
                     rename_files_folders(item)
 
 
